6d57e7d961d997f92d26000125c3aabcddbe68db
Atith794 <atith91098@gmail.com>
2024-04-12 07:27:19 +0530

    First commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React,{useEffect, useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[timer,ssetTimer] = useState(0);
+
+  useEffect(()=>{
+    let timerInterval = setInterval(()=>{
+      ssetTimer(time => time+1);
+    },1000)
+
+    return () => clearInterval(timerInterval);
+  },[]);
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p >You've been on this page {timer} seconds.</p>
     </div>
   )
 }
